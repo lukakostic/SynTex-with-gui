d49022c0be3eb82eecf234a92c10c191424cac0d
lukakostic <41348897+lukakostic@users.noreply.github.com>
2018-08-01 00:17:02 +0200

    Add files via upload

---
+++ SynTex/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            pictureBox1.Image = SynTex.Generate((Bitmap)pictureBox1.Image,method: comboBox1.Text, K: (int)numericUpDown1.Value, N: (int)numericUpDown2.Value, M: (int)numericUpDown3.Value, polish: (int)numericUpDown4.Value, t: (double)numericUpDown5.Value, OW:(int)(pictureBox1.Image.Width*numericUpDown6.Value),OH:(int)(pictureBox1.Image.Height * numericUpDown6.Value));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            pictureBox1.Image.Save(saveFileDialog1.FileName);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
